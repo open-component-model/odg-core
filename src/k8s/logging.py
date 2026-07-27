72f700194e5343a119761d8175a4ae948c1b2ceb
Chris Schneider <chris.schneider02@sap.com>
2026-07-27 10:08:16 +0000

    fix a crash on shutdown where json module was already unloaded, but formatMessage still called
    Signed-off-by: Chris Schneider <chris.schneider02@sap.com>

--- src/k8s/logging.py
+++ src/k8s/logging.py
@@ -283,6 +283,8 @@ def init_logging_thread(
 
 class JSONFormatter(logging.Formatter):
     def formatMessage(self, record) -> str:
+        if json is None:
+            return super().formatMessage(record)
         record.message = json.dumps(record.message)
         if (size := len(record.message.encode('utf-8'))) > 10000:
             record.message = f'"Request entity body is too large: {size} bytes"'
