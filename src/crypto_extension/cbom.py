ae2dc25425a28c8166b999d8dd76ef705b21572b
Jonas Brand (8R0WNI3) <j.brand@sap.com>
2026-08-13 10:11:26 +0200

    feat(ocm): Deduplicate artefact download implementation
    Signed-off-by: Jonas Brand (8R0WNI3) <j.brand@sap.com>

--- src/crypto_extension/cbom.py
+++ src/crypto_extension/cbom.py
@@ -5,7 +5,6 @@ import subprocess
 import tempfile
 
 import oci.client
-import oci.model
 import ocm
 
 import syft
@@ -60,7 +59,7 @@ def create_cbom(
 
 def find_cbom_or_create(
     component: ocm.Component,
-    access: ocm.Access,
+    resource: ocm.Resource,
     mapping: odg.extensions_cfg.CryptoMapping,
     oci_client: oci.client.Client,
     secret_factory: secret_mgmt.SecretFactory,
@@ -70,69 +69,24 @@ def find_cbom_or_create(
     creates a CBOM ad-hoc using `syft` and `cbomkit-theia`.
     """
     with tempfile.TemporaryDirectory(dir=own_dir) as tmp_dir:
-        filename_for_access_type = {
-            ocm.AccessType.LOCAL_BLOB: 'local_blob',
-            ocm.AccessType.OCI_BLOB: 'oci_blob',
-            ocm.AccessType.S3: 's3',
-        }
-
-        file_path = None
-        if filename := filename_for_access_type.get(access.type):
-            file_path = os.path.join(tmp_dir, filename)
+        file_path = os.path.join(tmp_dir, 'artefact')
 
         sbom_raw = syft.generate_raw_sbom_for_artefact(
             component=component,
-            access=access,
+            resource=resource,
             secret_factory=secret_factory,
             oci_client=oci_client,
-            aws_secret_name=mapping.aws_secret_name,
             file_path=file_path,
+            aws_secret_name=mapping.aws_secret_name,
         )
 
         sbom_path = os.path.join(tmp_dir, 'sbom')
         with open(sbom_path, 'w') as file:
             file.write(sbom_raw)
 
-        image_reference = None
-
-        if access.type is ocm.AccessType.OCI_REGISTRY:
-            access: ocm.OciAccess
-
-            image_reference = access.imageReference
-
-        elif access.type in (
-            ocm.AccessType.LOCAL_BLOB,
-            ocm.AccessType.OCI_BLOB,
-        ) and access.mediaType in (
-            oci.model.OCI_IMAGE_INDEX_MIME,
-            oci.model.OCI_MANIFEST_SCHEMA_V2_MIME,
-            oci.model.DOCKER_MANIFEST_LIST_MIME,
-            oci.model.DOCKER_MANIFEST_SCHEMA_V2_MIME,
-        ):
-            access: ocm.LocalBlobAccess | ocm.OciBlobAccess
-
-            image_reference = component.current_ocm_repo.component_version_oci_ref(
-                name=component.name,
-                version=component.version,
-            )
-
-            if access.type is ocm.AccessType.LOCAL_BLOB:
-                digest = access.localReference.lower()
-            else:
-                digest = access.digest.lower()
-
-            image_reference = str(oci.model.OciImageReference(image_reference).with_tag(digest))
-
-        if image_reference:
-            cbom = create_cbom(
-                image=image_reference,
-                sbom_path=sbom_path,
-            )
-
-        else:
-            cbom = create_cbom(
-                dir=file_path,
-                sbom_path=sbom_path,
-            )
+        return create_cbom(
+            dir=file_path,
+            sbom_path=sbom_path,
+        )
 
     return cbom
--- src/sbom_generator.py
+++ src/sbom_generator.py
@@ -19,7 +19,6 @@ import bdba_utils.scan
 import bdba_utils.util
 import k8s.util
 import k8s.logging
-import ocm
 import ocm.iter
 import odg.model
 import odg.util
@@ -50,26 +49,13 @@ def generate_sbom_with_syft(
 ) -> SBOM:
     logger.info(f'Creating SBOM for resource node {resource_node} using syft')
 
-    access = resource_node.resource.access
-
     with tempfile.TemporaryDirectory(dir=own_dir) as tmp_dir:
-        filename_for_access_type = {
-            ocm.AccessType.LOCAL_BLOB: 'local_blob',
-            ocm.AccessType.OCI_BLOB: 'oci_blob',
-            ocm.AccessType.OCI_REGISTRY: None,
-            ocm.AccessType.S3: 's3',
-        }
-
-        file_path = None
-        if filename := filename_for_access_type.get(access.type):
-            file_path = os.path.join(tmp_dir, filename)
-
         sbom_raw = syft.generate_raw_sbom_for_artefact(
             component=resource_node.component,
-            access=access,
+            resource=resource_node.resource,
             secret_factory=secret_factory,
             oci_client=oci_client,
-            file_path=file_path,
+            file_path=os.path.join(tmp_dir, 'artefact'),
             aws_secret_name=aws_secret_name,
             sbom_output_format=output_format,
         )
